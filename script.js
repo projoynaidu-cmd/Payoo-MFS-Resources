bf48dc8e37d9c3b3f72510a3efd1a296ed93a7b3
{username} <projoynaidu407@gmail.com>
2025-08-29 16:58:37 +0600

    add singup js and user find password when forget pin

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// console.log('Hello, World!');//this is a comment
+// Global user object
+const user = {
+    mobile: '01709437619',
+    password: '1234',
+};
 
-
-document.getElementById('login-btn')
+const loginBtn = document.getElementById('login-btn')
     .addEventListener('click', (e) => {
         //  console.log('Login button clicked!');
         const mobile = document.getElementById('mobile-number').value;
         const password = document.getElementById('pin-number').value;
         //console.log(mobile, password)
-        const user = {
-            mobile: '01709437619',
-            password: '1234',
-        }
+
         if (mobile === user.mobile && password === user.password) {
             localStorage.setItem('user', JSON.stringify(user)) //this will store the data in the local storage
             console.log(localStorage.getItem('user')); // this will print the data in the local storage
@@ -20,6 +20,26 @@ document.getElementById('login-btn')
         }
 
     })
+// singup btn 
+const signupBtn = document.getElementById('signup-btn')
+    .addEventListener('click', function () {
+       // window.location.href = 'signup.html'; // this will redirect to the signup page
+       alert('Signup button clicked!');
 
+    })
 
-    // forget btn 
+// forget btn 
+
+document.getElementById('forgot-pin-btn')
+    .addEventListener('click', function () {
+        // window.location.href = 'forgot-pin.html'; // this will redirect to the forgot pin page
+        //console.log('Forget PIN button clicked!');
+        const forgotModal = document.getElementById('forgot_modal'); // this will get the forgot modal
+        forgotModal.showModal(); // this will show the forgot modal
+
+        const resetBtn = document.getElementById('reset-btn'); // this will get the reset button
+        resetBtn.addEventListener('click', function () {
+            // explain how to get code/password from the user
+            alert('Your pin number is ' + user.password); // this will show an alert
+        })
+    })
