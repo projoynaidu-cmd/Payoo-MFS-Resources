6a16a24428addb69aa9e749e2dc6e337ead11a2c
{username} <projoynaidu407@gmail.com>
2025-08-30 17:42:11 +0600

    add froms toggle changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -14,11 +14,11 @@ document.getElementById('addMoney-btn')// this will get the add money button
         //console.log(bankName, accountNumber, addMoney, pinNumber, balanceEl);
 
         // validation
-        if(!bankName || !accountNumber || !pinNumber){
+        if (!bankName || !accountNumber || !pinNumber) {
             alert('Please fill all the fields!');
             return;
         }
-                // account number length check (must be 10 digits)
+        // account number length check (must be 10 digits)
         if (accountNumber.length !== 10) {
             alert('Account number must be 10 digits!');
             return;
@@ -31,13 +31,13 @@ document.getElementById('addMoney-btn')// this will get the add money button
         }
 
         // pin number check
-        if (pinNumber !== vaildPin) {
+        if (Number(pinNumber) !== vaildPin) {
             alert('Invalid PIN!');
             return;
         }
 
         // validation
-        if(addMoney <= 0){
+        if (addMoney <= 0) {
             alert('Please enter a valid amount!');
             return;
         }
@@ -47,5 +47,33 @@ document.getElementById('addMoney-btn')// this will get the add money button
         const newBalance = currentBalance + addMoney; // this will add the amount to the current balance
         balanceEl.innerText = newBalance; // this will update the balance
         alert('Money added successfully!');
-        
+
     })
+
+// Function to show form based on the clicked card
+const showForm = (formToShow) => {
+    // Hide all forms
+    const forms = document.querySelectorAll('.form-container');
+    forms.forEach(form => form.classList.add('hidden'));
+
+    // Show the selected form
+    if (formToShow) { // Check if formToShow is not null
+        formToShow.classList.remove('hidden');
+    }
+    console.log(formToShow);
+}
+
+// Event listener setup for each card
+const cards = document.querySelectorAll('.card');  // Select all cards
+cards.forEach(card => {
+    card.addEventListener('click', (e) => {
+        console.log(e.target.closest('.card'));
+        // Get the target form ID from the data-target attribute
+        const targetForm = document.getElementById(e.target.closest('.card').dataset.target);
+
+        // Show the corresponding form
+        showForm(targetForm);
+        console.log('clicked')
+    });
+    // console.log(card);
+});
